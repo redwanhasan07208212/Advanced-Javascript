64ef60bd2d7a37e9641b8e292de899408547143c
redwanhasan07208212 <redwanhasancse@gmail.com>
2021-04-22 13:07:55 +0600

    changing some element

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// if value equals to 0
+// empty string 
+// undefined for not defined any variable;
+// Null
+// NaN
+
+
+
+
+const age =NaN;
+
+if(age)
+{
+    console.log("This is true");
+}
+else
+{
+    console.log("This is false");
+}
