f45fdfcfc9a027abab80386efb0b718d9acf1161
redwanhasan07208212 <redwanhasancse@gmail.com>
2021-04-24 14:01:53 +0600

    double va tripple

---
+++ double-vs-trippleEqual.js
@@ -0,0 +1,36 @@
+// const first= 2;
+// const second ="2";
+// If we are using double equal the we get true statement because double equal just compare the value . Not the data type.
+
+// if(first == second)
+// {
+//     console.log("Condition is true");
+// }
+// else
+// {
+//     console.log("Condition is False");
+// }
+// const first= 2;
+// const second ="2";
+
+// if(first === second)
+// {
+//     console.log("Condition is true");
+// }
+// else
+// {
+//     console.log("Condition is False");
+// }
+//But tripple equal read the data type and also the value.
+const first= 1;
+const second =true;
+
+if(first === second)
+{
+    console.log("Condition is true");
+}
+else
+{
+    console.log("Condition is False");
+}
+// In double equal 1 is true condition so they mixed up with that.In tripple its not.
